6a4df15a085ff8d187193f88937367a151b7fd74
irenavilbik <97383041+irenavilbik@users.noreply.github.com>
2022-06-06 18:41:17 +0300

    Add files via upload

--- src/main/java/lt/bit/products/ui/controller/ProductController.java
+++ src/main/java/lt/bit/products/ui/controller/ProductController.java
@@ -1,65 +1,108 @@
 package lt.bit.products.ui.controller;
 
-import lt.bit.products.ui.model.Product;
-import lt.bit.products.ui.service.ProductService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import static org.springframework.util.StringUtils.hasLength;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
+import lt.bit.products.ui.model.Product;
+import lt.bit.products.ui.service.ProductService;
+import lt.bit.products.ui.service.SupplierService;
+import lt.bit.products.ui.service.UserService;
+import lt.bit.products.ui.service.error.ProductValidator;
+import lt.bit.products.ui.service.error.ValidationException;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-// beans can be added either via contructor or a class
 @Controller
 class ProductController {
 
-    private ProductService service;
+  private final ProductService service;
+  private final UserService userService;
+  private final SupplierService supplierService;
+  private final ProductValidator validator;
+  private final MessageSource messages;
 
-    ProductController(ProductService service) {
-        this.service = service;
-    }
+  ProductController(ProductService service, UserService userService,
+      SupplierService supplierService, ProductValidator validator,
+      MessageSource messages) {
+    this.service = service;
+    this.userService = userService;
+    this.supplierService = supplierService;
+    this.validator = validator;
+    this.messages = messages;
+  }
 
-    @GetMapping("/products")
-    String showProducts(Model model) {
-        List<Product> products = service.getProducts();
-        model.addAttribute("productItems", products);
-        return "productList";
-    }
-
-    @GetMapping("/products/{id}")
-    String editProduct(@PathVariable UUID id, Model model) {
-        model.addAttribute("productItem", service.getProduct(id));
-        return "productForm";
-    }
-
-    @GetMapping("/products/add")
-    String addProduct(Model model) {
-        model.addAttribute("productItem", new Product());
-        return "productForm";
+  @GetMapping("/products")
+  String showProducts(Model model, HttpServletRequest request) {
+    if (!userService.isAuthenticated()) {
+      return "login";
     }
 
-    @PostMapping("/products/save")
-    String saveProduct(@ModelAttribute Product product, Model model) {
-        String error = hasError(product.getName());
-        if (error != null){
-            model.addAttribute("errorMsg", error);
-            model.addAttribute("productItem", product);
-            return "productForm";
-        }
-        service.saveProduct(product);
-        return "redirect:/products";
+    String id = request.getParameter("id");
+    String name = request.getParameter("name");
+    List<Product> products;
+    if (hasLength(id) || hasLength(name)) {
+      products = service.findProducts(id, name);
+//      products = service.findProductsWithQuery(id, name);
+    } else {
+      products = service.getProducts();
     }
-    private String hasError(String name) {
-        if (!StringUtils.hasLength(name)){
-            return "Name id required";
-        } if (name.length() < 3) {
-            return "name is too short";
-        } return null;
+
+    model.addAttribute("searchCriteriaId", id);
+    model.addAttribute("searchCriteriaName", name);
+    model.addAttribute("productItems", products);
+    return "productList";
+  }
+
+  @GetMapping("/products/{id}")
+  String editProduct(@PathVariable UUID id, Model model) {
+    if (!userService.isAuthenticated()) {
+      return "login";
     }
-    @GetMapping("/products/delete")
-    String deleteProduct(@RequestParam UUID id) {
-        service.deleteProduct(id);
-        return "redirect:/products";
+    model.addAttribute("productItem", service.getProduct(id));
+    model.addAttribute("suppliers", supplierService.getSuppliers());
+    return "productForm";
+  }
+
+  @GetMapping("/products/add")
+  String addProduct(Model model) {
+    if (!userService.isAuthenticated()) {
+      return "login";
     }
+    model.addAttribute("productItem", new Product());
+    model.addAttribute("suppliers", supplierService.getSuppliers());
+    return "productForm";
+  }
+
+  @PostMapping("/products/save")
+  String saveProduct(@ModelAttribute Product product, Model model) throws ValidationException {
+    try {
+      validator.validate(product);
+    } catch (ValidationException e) {
+      model.addAttribute("errorMsg",
+          messages.getMessage("validation.error." + e.getCode(), e.getParams(),
+              Locale.getDefault()));
+      model.addAttribute("productItem", product);
+      return "productForm";
+    }
+    service.saveProduct(product);
+    return "redirect:/products";
+  }
+
+  @GetMapping("/products/delete")
+  String deleteProduct(@RequestParam UUID id) {
+    if (!userService.isAuthenticated()) {
+      return "login";
+    }
+    service.deleteProduct(id);
+    return "redirect:/products";
+  }
 }
---
+++ src/main/java/lt/bit/products/ui/controller/SupplierController.java
@@ -0,0 +1,65 @@
+package lt.bit.products.ui.controller;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
+import lt.bit.products.ui.model.Product;
+import lt.bit.products.ui.model.Supplier;
+import lt.bit.products.ui.service.SupplierService;
+import lt.bit.products.ui.service.UserService;
+import lt.bit.products.ui.service.error.ValidationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class SupplierController {
+
+  private final SupplierService service;
+  private final UserService userService;
+
+  public SupplierController(SupplierService service, UserService userService) {
+    this.service = service;
+    this.userService = userService;
+  }
+
+  @GetMapping("/suppliers")
+  String showProducts(Model model, HttpServletRequest request) {
+    if (!userService.isAuthenticated()) {
+      return "login";
+    }
+
+    List<Supplier> suppliers = service.getSuppliers();
+
+    model.addAttribute("supplierItems", suppliers);
+    return "supplierList";
+  }
+
+  @GetMapping("/suppliers/{id}")
+  String editSupplier(@PathVariable UUID id, Model model) {
+    if (!userService.isAuthenticated()) {
+      return "login";
+    }
+    model.addAttribute("supplierItem", service.getSupplier(id));
+    return "supplierForm";
+  }
+
+  @PostMapping("/suppliers/save")
+  String saveProduct(@ModelAttribute Supplier supplier, Model model) throws ValidationException {
+/*    try {
+      validator.validate(product);
+    } catch (ValidationException e) {
+      model.addAttribute("errorMsg",
+          messages.getMessage("validation.error." + e.getCode(), e.getParams(),
+              Locale.getDefault()));
+      model.addAttribute("productItem", product);
+      return "productForm";
+    }*/
+    service.saveSupplier(supplier);
+    return "redirect:/suppliers";
+  }
+}
